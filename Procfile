9593d058c21d34a4c29cc1720b2156ef10839b19
jorgeroyoa <jorge.royo@aborda.es>
2017-04-07 14:42:12 +0200

    fix

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-web:  node --harmony-async-await index.js
-worker: node worker.js
-release: node release.js
+web:  node index.js
--- index.js
+++ index.js
@@ -2,60 +2,6 @@ var express = require('express');
 var app = express();
 
 
-//var async = require('asyncawait/async');
-//var myawait = require('asyncawait/await');
-var _Promise = require('bluebird');
-
-var sumAsync = function(a, b) {
-	return new _Promise(function(resolve, reject) {
-		setTimeout(function() {
-			resolve(a+b);
-		}, 3000);
-	})
-}
-var sumAsync2 = function(a, b,c,cb) {
-		setTimeout(function() {
-		 	cb(null,a+b);
-		}, 3000);
-		
-}
-/*
-var suspendable = async(function(a, b) {
-	var result = myawait(sumAsync(a, b));
-	console.log("r: " + result);	
-	return result;
-});
-
-
-suspendable(2, 3).then(function(result) {
-	console.log("then: " + result)
-}).catch(function(err) {
-	console.log("err: " + err)
-})
-
-var as2 = _Promise.promisify(sumAsync2);
-as2(222, 3,4).then(function(result,arr) {
-	console.log("thenas2: " ,result,arr)
-}).catch(function(err) {
-	console.log("erras2: " + err)
-})
-*/
-
-
-async function suspendable2() {  
-  var pageContent
-  try {
-    pageContent = await sumAsync(5,6);
-     console.log("result1:" + pageContent);
-  } catch (ex) {
-    console.error("ex: " + ex)
-  }
- 
-  return pageContent
-}
-
-suspendable2();
-
 app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static(__dirname + '/public'));
